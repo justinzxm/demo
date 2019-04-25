f57686b660a38196906504608c208a34e3a7a840
zxm <2060197959@qq.com>
2019-04-25 11:44:39 +0800

    new featrue1

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -4,4 +4,7 @@ package com.example.demo;
  * Created by xingmin.zhang on 2019/4/25.
  */
 public class Test {
+    public static void main(String[] args) {
+        System.out.println("new featrue1");
+    }
 }
