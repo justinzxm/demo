c8294850eebeb048fbcd44080efb9188b8f03d9e
zxm <2060197959@qq.com>
2019-04-25 11:55:58 +0800

    new_release_v1.0

--- src/main/java/com/example/demo/Test.java
+++ src/main/java/com/example/demo/Test.java
@@ -6,5 +6,6 @@ package com.example.demo;
 public class Test {
     public static void main(String[] args) {
         System.out.println("new featrue1");
+        System.out.println("new_release_v1.0");
     }
 }
